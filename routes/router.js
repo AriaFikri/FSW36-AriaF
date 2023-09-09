3d9ea37e9e17684494834cd8689730ac9ced9548
Aria Fikri <ariasuandi@gmail.com>
2023-09-09 20:43:50 +0700

    add all page and partials

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,21 @@ const db = require('../db/db')
 route.get('/', (req, res) => {
     res.render('index.ejs')
 })
+route.get("/get", (req, res) => {
+  res.render("get.ejs");
+});
+route.get("/post", (req, res) => {
+  res.render("post.ejs");
+});
+route.get("/patch", (req, res) => {
+  res.render("patch.ejs");
+});
+route.get("/delete", (req, res) => {
+  res.render("delete.ejs");
+});
+route.get("/put", (req, res) => {
+  res.render("put.ejs");
+});
 // GET (READ)
 route.get("/all", control.getAll);
 route.get("/search", control.getSearch);
